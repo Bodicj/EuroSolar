9be998265ad7685365d8899c794b7392c1e568c0
Bodicj <Bodicj@gmail.com>
2017-08-15 11:20:48 +0300

    Add click handlers for all buttons

--- js/form.js
+++ js/form.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
         if ($('#private')[0].checked == true) {
             $('#entity_form').css("display", 'none');
             $('#private_form').css("display", "flex");
-            $("#btn_request")[0].setAttribute("form","private_form");
         }
         else {
             $('#private_form').css("display", "none");
             $('#entity_form').css("display", 'flex');
-            $("#btn_request")[0].setAttribute("form","entity_form");
         }
     });
 });
