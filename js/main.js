e73de3b0d1b2b7c118b7c882f208d51aff9c4f93
Bodicj <Bodicj@gmail.com>
2017-08-16 12:10:31 +0300

    Add adaptive part

--- js/main.js
+++ js/main.js
@@ -5,6 +5,24 @@ $('.carousel').slick({
 
     responsive: [{
         breakpoint: 900,
-        settings: {slidesToShow: 2,}
-    }]
+        settings: {slidesToShow: 2}
+    },
+    {
+        breakpoint: 425,
+        settings: {slidesToShow: 1}
+    }
+    ]
+});
+
+$(document).ready(function () {
+    if(window.matchMedia('(max-width: 425px)').matches) {
+        $('.service').hover(
+            function () {
+                $(this).css("max-height", "354px");
+            },
+            function () {
+                $(this).css("max-height", "106px");
+            }
+        )
+    }
 });
